f41bae6baf4f351c8309c49c490393ef2d98d17d
Larisse Lima <larisse.lima2@gmail.com>
2022-01-31 19:38:55 -0300

    Eventos do input e box da imagem

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
 
 function GlobalStyle() {
@@ -47,72 +49,83 @@ function Titulo(props) {
 }
 
 export default function PaginaInicial() {
-  const username = 'LarisseLima';
+  const [username, setUsername] = React.useState('omariosouto');
+  const roteamento = useRouter();
 
   return (
-    <>
-      <GlobalStyle />
-      <Box
-        styleSheet={{
-          display: 'flex', alignItems: 'center', justifyContent: 'center',
-          backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage: 'url(https://images.unsplash.com/photo-1604430352940-cf2acfb26ea9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80)',
-          backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
-        }}
-      >
-        <Box
-          styleSheet={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            flexDirection: {
-              xs: 'column',
-              sm: 'row',
-            },
-            width: '100%', maxWidth: '700px',
-            borderRadius: '5px', padding: '32px', margin: '16px',
-            boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-            backgroundColor: appConfig.theme.colors.neutrals[700],
-          }}
-        >
-          {}
+      <>
           <Box
-            as="form"
-            styleSheet={{
-              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-              width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
-            }}
+              styleSheet={{
+                  display: 'flex', alignItems: 'center', justifyContent: 'center',
+                  backgroundColor: appConfig.theme.colors.primary[500],
+                  backgroundImage: 'url(https://images.unsplash.com/photo-1604430352940-cf2acfb26ea9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80)',
+                  backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+              }}
           >
-            <Titulo tag="h2">Boas vindas de volta!</Titulo>
-            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
-              {appConfig.name}
-            </Text>
+              <Box
+                  styleSheet={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'space-between',
+                      flexDirection: {
+                          xs: 'column',
+                          sm: 'row',
+                      },
+                      width: '100%', maxWidth: '700px',
+                      borderRadius: '5px', padding: '32px', margin: '16px',
+                      boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                      backgroundColor: appConfig.theme.colors.neutrals[700],
+                  }}
+              >
+                  {}
+                  <Box
+                      as="form"
+                      onSubmit={function (infosDoEvento) {
+                          infosDoEvento.preventDefault();
+                          console.log('Alguém submeteu o form');
+                          roteamento.push('/chat');
+                      }}
+                      styleSheet={{
+                          display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                          width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+                      }}
+                  >
+                      <Titulo tag="h2">Boas vindas de volta!</Titulo>
+                      <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+                          {appConfig.name}
+                      </Text>
 
-            <TextField
-              fullWidth
-              textFieldColors={{
-                neutral: {
-                  textColor: appConfig.theme.colors.neutrals[200],
-                  mainColor: appConfig.theme.colors.neutrals[900],
-                  mainColorHighlight: appConfig.theme.colors.primary[500],
-                  backgroundColor: appConfig.theme.colors.neutrals[800],
-                },
-              }}
-            />
-            <Button
-              type='submit'
-              label='Entrar'
-              fullWidth
-              buttonColors={{
-                contrastColor: appConfig.theme.colors.neutrals["000"],
-                mainColor: appConfig.theme.colors.primary[500],
-                mainColorLight: appConfig.theme.colors.primary[400],
-                mainColorStrong: appConfig.theme.colors.primary[600],
-              }}
-            />
-          </Box>
-          {}
-
+                      {}
+                      <TextField
+                          value={username}
+                          onChange={function (event) {
+                              console.log('usuario digitou', event.target.value);
+                              const valor = event.target.value;
+                              setUsername(valor);
+                          }}
+                          fullWidth
+                          textFieldColors={{
+                              neutral: {
+                                  textColor: appConfig.theme.colors.neutrals[200],
+                                  mainColor: appConfig.theme.colors.neutrals[900],
+                                  mainColorHighlight: appConfig.theme.colors.primary[500],
+                                  backgroundColor: appConfig.theme.colors.neutrals[800],
+                              },
+                          }}
+                      />
+                      <Button
+                          type='submit'
+                          label='Entrar'
+                          fullWidth
+                          buttonColors={{
+                              contrastColor: appConfig.theme.colors.neutrals["000"],
+                              mainColor: appConfig.theme.colors.primary[500],
+                              mainColorLight: appConfig.theme.colors.primary[400],
+                              mainColorStrong: appConfig.theme.colors.primary[600],
+                          }}
+                      />
+                  </Box>
+                  {}
 
           {}
           <Box
