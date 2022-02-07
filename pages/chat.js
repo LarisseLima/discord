821c52c8c53b90252d7633c3b47ab28310811c1a
Larisse Lima <larisse.lima2@gmail.com>
2022-02-07 20:39:21 -0300

    adiciona page 404

--- pages/chat.js
+++ pages/chat.js
@@ -10,295 +10,295 @@ const SUPABASE_URL = 'https://eioxvajedyzmwhgvgjwn.supabase.co';
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 function escutaMensagensEmTempoReal(adicionaMensagem) {
-    return supabaseClient
-        .from('mensagens')
-        .on('INSERT', (respostaLive) => {
-            adicionaMensagem(respostaLive.new);
-        })
-        .subscribe();
+ return supabaseClient
+  .from('mensagens')
+  .on('INSERT', (respostaLive) => {
+   adicionaMensagem(respostaLive.new);
+  })
+  .subscribe();
 }
 
 export default function ChatPage() {
-    const router = useRouter();
-    const usuarioLogado = router.query.username;
-    const [mensagem, setMensagem] = React.useState('');
-    const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
+ const router = useRouter();
+ const usuarioLogado = router.query.username;
+ const [mensagem, setMensagem] = React.useState('');
+ const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
 
-    React.useEffect(() => {
-        supabaseClient
-            .from('mensagens')
-            .select('*')
-            .order('id', { ascending: false })
-            .then(({ data }) => {
-                setListaDeMensagens(data);
-            });
+ React.useEffect(() => {
+  supabaseClient
+   .from('mensagens')
+   .select('*')
+   .order('id', { ascending: false })
+   .then(({ data }) => {
+    setListaDeMensagens(data);
+   });
 
-        const subscription = escutaMensagensEmTempoReal((novaMensagem) => {
-            console.log('Nova mensagem:', novaMensagem);
-            console.log('listaDeMensagens:', listaDeMensagens);
-            setListaDeMensagens((valorAtualDaLista) => {
-                console.log('valorAtualDaLista:', valorAtualDaLista);
-                return [
-                    novaMensagem,
-                    ...valorAtualDaLista,
-                ]
-            });
-        });
+  const subscription = escutaMensagensEmTempoReal((novaMensagem) => {
+   console.log('Nova mensagem:', novaMensagem);
+   console.log('listaDeMensagens:', listaDeMensagens);
+   setListaDeMensagens((valorAtualDaLista) => {
+    console.log('valorAtualDaLista:', valorAtualDaLista);
+    return [
+     novaMensagem,
+     ...valorAtualDaLista,
+    ]
+   });
+  });
 
-        return () => {
-            subscription.unsubscribe();
+  return () => {
+   subscription.unsubscribe();
+  }
+ }, []);
+
+ function handleNovaMensagem(novaMensagem) {
+  const mensagem = {
+   de: usuarioLogado,
+   texto: novaMensagem,
+  };
+
+  supabaseClient
+   .from('mensagens')
+   .insert([
+    mensagem
+   ])
+   .then(({ data }) => {
+    console.log('Criando mensagem: ', data);
+   });
+
+  setMensagem('');
+ }
+
+ function handleDeletaMensagem(mensagemAtual) {
+  supabaseClient
+   .from("mensagens")
+   .delete()
+   .match({ id: mensagemAtual.id })
+   .then(({ data }) => {
+    const listaDeMensagensFiltrada = listaDeMensagens.filter((mensagem) => {
+     return mensagem.id != data[0].id;
+    });
+    setListaDeMensagens(listaDeMensagensFiltrada);
+   });
+ }
+
+ return (
+  <Box
+   styleSheet={{
+    display: 'flex', alignItems: 'center', justifyContent: 'center',
+    backgroundColor: appConfig.theme.colors.neutrals[200],
+    backgroundImage: 'url(https://images.unsplash.com/photo-1604430352940-cf2acfb26ea9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80)',
+    backgroundRepeat: 'no-repeat', backgroundSize: 'cover',
+    color: appConfig.theme.colors.neutrals['000']
+   }}
+  >
+   <Box
+    styleSheet={{
+     display: 'flex',
+     flexDirection: 'column',
+     flex: 1,
+     boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+     borderRadius: '10px',
+     backgroundColor: appConfig.theme.colors.neutrals[100],
+     height: '100%',
+     maxWidth: '95%',
+     maxHeight: '95vh',
+     padding: '32px',
+    }}
+   >
+    <Header />
+    <Box
+     styleSheet={{
+      position: 'relative',
+      display: 'flex',
+      flex: 1,
+      height: '80%',
+      backgroundColor: appConfig.theme.colors.neutrals[100],
+      flexDirection: 'column',
+      borderRadius: '5px',
+      padding: '16px',
+     }}
+    >
+     <MessageList
+      mensagens={listaDeMensagens}
+      handleDeletaMensagem={handleDeletaMensagem}
+     />
+     { }
+     <Box
+      as="form"
+      styleSheet={{
+       display: 'flex',
+       alignItems: 'center',
+      }}
+     >
+      <TextField
+       value={mensagem}
+       onChange={(event) => {
+        const valor = event.target.value;
+        setMensagem(valor);
+       }}
+       onKeyPress={(event) => {
+        if (event.key === 'Enter') {
+         event.preventDefault();
+         handleNovaMensagem(mensagem);
         }
-    }, []);
+       }}
+       placeholder="Insira sua mensagem aqui..."
+       type="textarea"
+       styleSheet={{
+        width: '100%',
+        border: '0',
+        resize: 'none',
+        borderRadius: '5px',
+        padding: '15px 8px',
+        marginRight: '12px',
+        backgroundColor: appConfig.theme.colors.primary[800],
+        color: appConfig.theme.colors.neutrals["000"],
+       }}
+      />
+      { }
+      <ButtonSendSticker
+       onStickerClick={(sticker) => {
+        handleNovaMensagem(':sticker: ' + sticker);
+       }}
+      />
+      <Button styleSheet={{
+       borderRadius: '50%',
+       minWidth: '50px',
+       minHeight: '50px',
+       fontSize: '20px',
+       marginBottom: '8px',
+       margin: '5px',
+       lineHeight: '',
+       display: 'flex',
+       alignItems: 'center',
+       justifyContent: 'center',
+      }}
+       iconName="FaPaperPlane"
+       type="button"
+       variant='secondary'
+       colorVariant='positive'
 
-    function handleNovaMensagem(novaMensagem) {
-        const mensagem = {
-            de: usuarioLogado,
-            texto: novaMensagem,
-        };
-
-        supabaseClient
-            .from('mensagens')
-            .insert([
-                mensagem
-            ])
-            .then(({ data }) => {
-                console.log('Criando mensagem: ', data);
-            });
-
-        setMensagem('');
-    }
-
-    function handleDeletaMensagem(mensagemAtual) {
-        supabaseClient
-            .from("mensagens")
-            .delete()
-            .match({ id: mensagemAtual.id })
-            .then(({ data }) => {
-                const listaDeMensagensFiltrada = listaDeMensagens.filter((mensagem) => {
-                    return mensagem.id != data[0].id;
-                });
-                setListaDeMensagens(listaDeMensagensFiltrada);
-            });
-    }
-
-    return (
-        <Box
-            styleSheet={{
-                display: 'flex', alignItems: 'center', justifyContent: 'center',
-                backgroundColor: appConfig.theme.colors.neutrals[200],
-                backgroundImage: 'url(https://images.unsplash.com/photo-1604430352940-cf2acfb26ea9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80)',
-                backgroundRepeat: 'no-repeat', backgroundSize: 'cover',
-                color: appConfig.theme.colors.neutrals['000']
-            }}
-        >
-            <Box
-                styleSheet={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    flex: 1,
-                    boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-                    borderRadius: '10px',
-                    backgroundColor: appConfig.theme.colors.neutrals[100],
-                    height: '100%',
-                    maxWidth: '95%',
-                    maxHeight: '95vh',
-                    padding: '32px',
-                }}
-            >
-                <Header />
-                <Box
-                    styleSheet={{
-                        position: 'relative',
-                        display: 'flex',
-                        flex: 1,
-                        height: '80%',
-                        backgroundColor: appConfig.theme.colors.neutrals[100],
-                        flexDirection: 'column',
-                        borderRadius: '5px',
-                        padding: '16px',
-                    }}
-                >
-                    <MessageList
-                        mensagens={listaDeMensagens}
-                        handleDeletaMensagem={handleDeletaMensagem}
-                    />
-                    {}
-                    <Box
-                        as="form"
-                        styleSheet={{
-                            display: 'flex',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <TextField
-                            value={mensagem}
-                            onChange={(event) => {
-                                const valor = event.target.value;
-                                setMensagem(valor);
-                            }}
-                            onKeyPress={(event) => {
-                                if (event.key === 'Enter') {
-                                    event.preventDefault();
-                                    handleNovaMensagem(mensagem);
-                                }
-                            }}
-                            placeholder="Insira sua mensagem aqui..."
-                            type="textarea"
-                            styleSheet={{
-                                width: '100%',
-                                border: '0',
-                                resize: 'none',
-                                borderRadius: '5px',
-                                padding: '15px 8px',
-                                marginRight: '12px',
-                                backgroundColor: appConfig.theme.colors.primary[800],
-                                color: appConfig.theme.colors.neutrals["000"],
-                            }}
-                        />
-                        {}
-                        <ButtonSendSticker
-                            onStickerClick={(sticker) => {
-                                handleNovaMensagem(':sticker: ' + sticker);
-                            }}
-                        />
-                        <Button styleSheet={{
-                            borderRadius: '50%',
-                            minWidth: '50px',
-                            minHeight: '50px',
-                            fontSize: '20px',
-                            marginBottom: '8px',
-                            margin: '5px',
-                            lineHeight: '',
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                        }}
-                            iconName="FaPaperPlane"
-                            type="button"
-                            variant='secondary'
-                            colorVariant='positive'
-
-                            onClick={(event) => {
-                                event.preventDefault();
-                                handleNovaMensagem(mensagem);
-                            }}
-                        />
-                    </Box>
-                </Box>
-            </Box>
-        </Box>
-    )
+       onClick={(event) => {
+        event.preventDefault();
+        handleNovaMensagem(mensagem);
+       }}
+      />
+     </Box>
+    </Box>
+   </Box>
+  </Box>
+ )
 }
 
 function Header() {
-    return (
-        <>
-            <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
-                <Text variant='heading4'>
-                    CHAT
-                </Text>
-                <Button
-                    variant='secondary'
-                    colorVariant='light'
-                    label='Logout'
-                    href="/"
-                />
-            </Box>
-        </>
-    )
+ return (
+  <>
+   <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
+    <Text variant='heading4'>
+     CHAT
+    </Text>
+    <Button
+     variant='secondary'
+     colorVariant='light'
+     label='Logout'
+     href="/"
+    />
+   </Box>
+  </>
+ )
 }
 
 function MessageList(props) {
-    console.log("MessageList", props);
+ console.log("MessageList", props);
 
-    const handleDeletaMensagem = props.handleDeletaMensagem;
+ const handleDeletaMensagem = props.handleDeletaMensagem;
+ return (
+  <Box
+   tag="ul"
+   styleSheet={{
+    overflow: 'auto',
+    display: 'flex',
+    flexDirection: 'column-reverse',
+    flex: 1,
+    color: appConfig.theme.colors.neutrals["000"],
+    marginBottom: '16px',
+   }}
+  >
+   {props.mensagens.map((mensagem) => {
     return (
-        <Box
-            tag="ul"
-            styleSheet={{
-                overflow: 'auto',
-                display: 'flex',
-                flexDirection: 'column-reverse',
-                flex: 1,
-                color: appConfig.theme.colors.neutrals["000"],
-                marginBottom: '16px',
-            }}
-        >
-            {props.mensagens.map((mensagem) => {
-                return (
-                    <Text
-                        key={mensagem.id}
-                        tag="li"
-                        styleSheet={{
-                            borderRadius: '5px',
-                            padding: '6px',
-                            marginBottom: '12px',
-                            hover: {
-                                backgroundColor: appConfig.theme.colors.neutrals[100],
-                            }
-                        }}
-                    >
-                        <Box
-                            styleSheet={{
-                                marginBottom: '8px',
-                            }}
-                        >
-                            <Image
-                                styleSheet={{
-                                    width: '20px',
-                                    height: '20px',
-                                    borderRadius: '50%',
-                                    display: 'inline-block',
-                                    marginRight: '8px',
-                                }}
-                                src={`https://github.com/${mensagem.de}.png`}
-                            />
-                            <Text tag="strong">
-                                {mensagem.de}
-                            </Text>
-                            <Text
-                                styleSheet={{
-                                    fontSize: '10px',
-                                    marginLeft: '8px',
-                                    color: appConfig.theme.colors.neutrals[200],
-                                }}
-                                tag="span"
-                            >
-                                {new Date().toLocaleDateString("pt-br", { hour: "numeric", minute: "numeric", second: "numeric" })}
-                            </Text>
+     <Text
+      key={mensagem.id}
+      tag="li"
+      styleSheet={{
+       borderRadius: '5px',
+       padding: '6px',
+       marginBottom: '12px',
+       hover: {
+        backgroundColor: appConfig.theme.colors.neutrals[100],
+       }
+      }}
+     >
+      <Box
+       styleSheet={{
+        marginBottom: '8px',
+       }}
+      >
+       <Image
+        styleSheet={{
+         width: '20px',
+         height: '20px',
+         borderRadius: '50%',
+         display: 'inline-block',
+         marginRight: '8px',
+        }}
+        src={`https://github.com/${mensagem.de}.png`}
+       />
+       <Text tag="strong">
+        {mensagem.de}
+       </Text>
+       <Text
+        styleSheet={{
+         fontSize: '10px',
+         marginLeft: '8px',
+         color: appConfig.theme.colors.neutrals[200],
+        }}
+        tag="span"
+       >
+        {new Date().toLocaleDateString("pt-br", { hour: "numeric", minute: "numeric", second: "numeric" })}
+       </Text>
 
-                            <Icon
-                                name={"FaTrash"}
-                                styleSheet={{
-                                    marginLeft: "12px",
-                                    width: "15px",
-                                    height: "15px",
-                                    color: appConfig.theme.colors.primary["100"],
-                                    hover: {
-                                        color: "orange",
-                                    },
-                                    display: "inline-block",
-                                }}
-                                onClick={(event) => {
-                                    event.preventDefault();
-                                    handleDeletaMensagem(mensagem);
-                                }}
-                            />
+       <Icon
+        name={"FaTrash"}
+        styleSheet={{
+         marginLeft: "12px",
+         width: "15px",
+         height: "15px",
+         color: appConfig.theme.colors.primary["100"],
+         hover: {
+          color: "orange",
+         },
+         display: "inline-block",
+        }}
+        onClick={(event) => {
+         event.preventDefault();
+         handleDeletaMensagem(mensagem);
+        }}
+       />
 
-                        </Box>
+      </Box>
 
-                        {mensagem.texto.startsWith(':sticker:')
-                                ? (
-                                    <Image src={mensagem.texto.replace(':sticker:', '')} />
-                                )
-                                : (
-                                    mensagem.texto
-                                )}
-                        {}
-                        {}
-                    </Text>
-                );
-            })}
-        </Box>
+      {mensagem.texto.startsWith(':sticker:')
+       ? (
+        <Image src={mensagem.texto.replace(':sticker:', '')} />
+       )
+       : (
+        mensagem.texto
+       )}
+      { }
+      { }
+     </Text>
     );
+   })}
+  </Box>
+ );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import { useRouter } from 'next/router';
 import appConfig from '../config.json';
 
 function Title(props) {
-  const Tag = props.tag || 'h1';
-  return (
-    <>
-      <Tag>{props.children}</Tag>
-      <style jsx>{`
+ const Tag = props.tag || 'h1';
+ return (
+  <>
+   <Tag>{props.children}</Tag>
+   <style jsx>{`
             ${Tag} {
                 color: ${appConfig.theme.colors.neutrals['000']};
                 font-size: 28px;
@@ -16,135 +16,135 @@ function Title(props) {
                 font-family: 'Ink Free';
             }
             `}</style>
-    </>
-  );
+  </>
+ );
 }
 
 export default function PaginaInicial() {
-  const [username, setUsername] = React.useState('');
-  const validateInput = username.length > 2
-  const router = useRouter();
+ const [username, setUsername] = React.useState('');
+ const validateInput = username.length > 2
+ const router = useRouter();
 
-  return (
-    <>
-      <Box
-        styleSheet={{
-          display: 'flex', alignItems: 'center', justifyContent: 'center',
-          backgroundImage: 'url(https://images.unsplash.com/photo-1604430352940-cf2acfb26ea9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80)',
-          backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
-        }}
+ return (
+  <>
+   <Box
+    styleSheet={{
+     display: 'flex', alignItems: 'center', justifyContent: 'center',
+     backgroundImage: 'url(https://images.unsplash.com/photo-1604430352940-cf2acfb26ea9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80)',
+     backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+    }}
+   >
+    <Box
+     styleSheet={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      flexDirection: {
+       xs: 'column',
+       sm: 'row',
+      },
+      width: '100%', maxWidth: '700px',
+      borderRadius: '5px', padding: '32px', margin: '16px',
+      boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+      backgroundColor: appConfig.theme.colors.neutrals[100],
+     }}
+    >
+     { }
+     <Box
+      as="form"
+      onSubmit={function (event) {
+       event.preventDefault()
+       router.push(`/chat?username=${username}`)
+      }}
+      styleSheet={{
+       display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+       width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+      }}
+     >
+      <Title tag="h2">Welcome Back!</Title>
+      <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[200] }}>
+       {appConfig.name}
+      </Text>
+
+      { }
+
+      <TextField
+       value={username}
+       onChange={function (event) {
+        const newValue = event.target.value;
+        setUsername(newValue)
+       }}
+       placeholder='Digite o seu usuário do Github...'
+       fullWidth
+       textFieldColors={{
+        neutral: {
+         textColor: appConfig.theme.colors.neutrals["000"],
+         mainColor: appConfig.theme.colors.primary[500],
+         mainColorHighlight: appConfig.theme.colors.neutrals["000"],
+         backgroundColor: appConfig.theme.colors.neutrals[100],
+        },
+       }}
+      />
+      <Button
+       type='submit'
+       label='Login'
+       fullWidth
+       buttonColors={{
+        contrastColor: appConfig.theme.colors.neutrals["000"],
+        mainColor: appConfig.theme.colors.neutrals[100],
+        mainColorLight: appConfig.theme.colors.primary[100],
+        mainColorStrong: appConfig.theme.colors.primary[500],
+       }}
+      />
+     </Box>
+     { }
+
+
+     { }
+     <Box
+      styleSheet={{
+       display: 'flex',
+       flexDirection: 'column',
+       alignItems: 'center',
+       maxWidth: '200px',
+       padding: '16px',
+       backgroundColor: appConfig.theme.colors.neutrals[100],
+       border: '1px solid',
+       borderColor: appConfig.theme.colors.neutrals[400],
+       borderRadius: '10px',
+       flex: 1,
+       minHeight: '240px',
+      }}
+     >
+      <Image
+       styleSheet={{
+        borderRadius: '50%',
+        marginBottom: '16px',
+       }}
+       src={validateInput ? `https://github.com/${username}.png` : 'https://i.pinimg.com/originals/11/8e/6f/118e6f39fac9344d6589c84d5ee9e667.png'}
+
+      />
+      <Text
+       variant="body4"
+       tag={validateInput ? "a" : "span"}
+       target="_blank"
+       href={`https://github.com/${username}`}
+       styleSheet={{
+        color: appConfig.theme.colors.neutrals[200],
+        backgroundColor: appConfig.theme.colors.primary[999],
+        border: '1px solid',
+        borderColor: appConfig.theme.colors.neutrals[400],
+        borderRadius: '10px',
+        padding: '3px 10px',
+        borderRadius: '1000px',
+       }}
       >
-        <Box
-          styleSheet={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            flexDirection: {
-              xs: 'column',
-              sm: 'row',
-            },
-            width: '100%', maxWidth: '700px',
-            borderRadius: '5px', padding: '32px', margin: '16px',
-            boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-            backgroundColor: appConfig.theme.colors.neutrals[100],
-          }}
-        >
-          { }
-          <Box
-            as="form"
-            onSubmit={function (event) {
-              event.preventDefault()
-              router.push(`/chat?username=${username}`)
-            }}
-            styleSheet={{
-              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-              width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
-            }}
-          >
-            <Title tag="h2">Welcome Back!</Title>
-            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[200] }}>
-              {appConfig.name}
-            </Text>
-
-            { }
-
-            <TextField
-              value={username}
-              onChange={function (event) {
-                const newValue = event.target.value;
-                setUsername(newValue)
-              }}
-              placeholder='Digite o seu usuário do Github...'
-              fullWidth
-              textFieldColors={{
-                neutral: {
-                  textColor: appConfig.theme.colors.neutrals["000"],
-                  mainColor: appConfig.theme.colors.primary[500],
-                  mainColorHighlight: appConfig.theme.colors.neutrals["000"],
-                  backgroundColor: appConfig.theme.colors.neutrals[100],
-                },
-              }}
-            />
-            <Button
-              type='submit'
-              label='Login'
-              fullWidth
-              buttonColors={{
-                contrastColor: appConfig.theme.colors.neutrals["000"],
-                mainColor: appConfig.theme.colors.neutrals[100],
-                mainColorLight: appConfig.theme.colors.primary[100],
-                mainColorStrong: appConfig.theme.colors.primary[500],
-              }}
-            />
-          </Box>
-          { }
-
-
-          { }
-          <Box
-            styleSheet={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              maxWidth: '200px',
-              padding: '16px',
-              backgroundColor: appConfig.theme.colors.neutrals[100],
-              border: '1px solid',
-              borderColor: appConfig.theme.colors.neutrals[400],
-              borderRadius: '10px',
-              flex: 1,
-              minHeight: '240px',
-            }}
-          >
-            <Image
-              styleSheet={{
-                borderRadius: '50%',
-                marginBottom: '16px',
-              }}
-              src={validateInput ? `https://github.com/${username}.png` : 'https://i.pinimg.com/originals/11/8e/6f/118e6f39fac9344d6589c84d5ee9e667.png'}
-
-            />
-            <Text
-              variant="body4"
-              tag={validateInput ? "a" : "span"}
-              target="_blank"
-              href={`https://github.com/${username}`}
-              styleSheet={{
-                color: appConfig.theme.colors.neutrals[200],
-                backgroundColor: appConfig.theme.colors.primary[999],
-                border: '1px solid',
-                borderColor: appConfig.theme.colors.neutrals[400],
-                borderRadius: '10px',
-                padding: '3px 10px',
-                borderRadius: '1000px',
-              }}
-            >
-              {username || "GitHub"}
-            </Text>
-          </Box>
-          { }
-        </Box>
-      </Box>
-    </>
-  );
+       {username || "GitHub"}
+      </Text>
+     </Box>
+     { }
+    </Box>
+   </Box>
+  </>
+ );
 }
